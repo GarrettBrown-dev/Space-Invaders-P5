a10b3f23bfa7dd46c7a59075031cbc361c7967a8
Garrett <gman9mm@live.com>
2021-04-01 14:23:09 -0700

    Add drops on spacebar.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 var ship;
 var flowers = [];
+var drops = [];
 
 function setup() {
   createCanvas(600, 600);
   ship = new Ship();
-  drop = new Drop(width / 2, height / 2);
+  // drop = new Drop(width / 2, height / 2);
   for (var i = 0; i < 6; i++) {
     flowers[i] = new Flower(i * 80 + 80, 60);
   }
@@ -13,14 +14,20 @@ function setup() {
 function draw() {
   background(51);
   ship.show();
-  drop.show();
-  drop.move();
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].show();
+    drops[i].move();
+  }
   for (var i = 0; i < flowers.length; i++) {
     flowers[i].show();
   }
 }
 
 function keyPressed() {
+  if (key === " ") {
+    var drop = new Drop(width / 2, height / 2);
+    drops.push(drop);
+  }
   if (keyCode === RIGHT_ARROW) {
     ship.move(1);
   } else if (keyCode === LEFT_ARROW) {
